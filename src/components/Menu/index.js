96cecb7157301beb2d49360555a60b6983c05530
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-11-09 17:50:43 +0100

    :lipstick: menu nav bar finit ou presque

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import styles from './Menu.module.css'
+import logoHome from './img/home.svg'
+import logoResearch from './img/research.svg'
+import logoTop from './img/top.svg'
+import logoUser from './img/user.svg'
 
 const Menu = () => {
   const navStyle = {
@@ -9,10 +13,10 @@ const Menu = () => {
   return (
     <div className={styles.menu_container}>
         <ul className={styles.menu_list}>
-            <li><Link className={styles.link} to='/'>Accueil</Link></li>
-            <li><Link className={styles.link} to='/Recherche'>Rechercher</Link></li>
-            <li><Link className={styles.link} to='/Top10'>Top 10</Link></li>
-            <li><Link className={styles.link} to='/Compte'>Compte</Link></li>
+            <li><img src={logoHome}/><Link className={styles.link} to='/'>Accueil</Link></li>
+            <li><img src={logoResearch}/><Link className={styles.link} to='/Recherche'>Rechercher</Link></li>
+            <li><img src={logoTop}/><Link className={styles.link} to='/Top10'>Top 10</Link></li>
+            <li><img src={logoUser}/><Link className={styles.link} to='/Compte'>Compte</Link></li>
         </ul>
     </div>
         
