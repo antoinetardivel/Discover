2d0e91a156ac7eccd609fa035da75f6adae834cf
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-11-09 17:35:35 +0100

    :lipstick: debut de la nav bar

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styles from './Menu.module.css'
 
 const Menu = () => {
   const navStyle = {
     color: 'black'
   }
   return (
-    <div className="nav-container">
-            <div className='nav'>
-                <ul className='navLinks'>
-                    <li><Link style={navStyle} to='/'>Accueil</Link></li>
-                    <li><Link style={navStyle} to='/Recherche'>Rechercher</Link></li>
-                    <li><Link style={navStyle} to='/Top10'>Top 10</Link></li>
-                    <li><Link style={navStyle} to='/Compte'>Compte</Link></li>
-                </ul>
-            </div>
-        </div>
+    <div className={styles.menu_container}>
+        <ul className={styles.menu_list}>
+            <li><Link className={styles.link} to='/'>Accueil</Link></li>
+            <li><Link className={styles.link} to='/Recherche'>Rechercher</Link></li>
+            <li><Link className={styles.link} to='/Top10'>Top 10</Link></li>
+            <li><Link className={styles.link} to='/Compte'>Compte</Link></li>
+        </ul>
+    </div>
+        
   )
 }
 
