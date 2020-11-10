b0c4a501ba2cbcce439660d3bd64d513b0804849
jmagry <justine.magry@mmibordeaux.com>
2020-11-10 16:52:10 +0100

    barre recherche fonctionnelle

--- src/pages/SearchPage/Search.js
+++ src/pages/SearchPage/Search.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import style from './Search.module.css'
 import Loupe from 'components/Menu/img/research.svg'
+import MusicIcon from 'components/MusicIcon/MusicIcon'
+import ImageMusicMoody from 'components/MusicIcon/img/moody.png'
 
 function Search() {
     return(
-        <div>
-           <div className={style.SearchHierarchy}>
-                    <div className={style.bar}>
-                    <img className={style.Loupe}src={Loupe}/>
-                    <h1 className={style.titre}>Recherche</h1>
+           <div className={style.PageSearch}>
+
+                    <div className={style.searchHeader}>
+                        <img className={style.searchHeader_loupe} src={Loupe}/>
+                        <input className={style.searchHeader_input} type="text" id="rechercher" name="rechercher" placeholder="Rechercher"/>
                     </div>
 
                     <div className={style.content}>
+                        <div className={style.content_result}>
+                            <div className={style.content_result_ligne}>
+                            </div>
+                        </div>
                         
                     </div>
            </div>
             
-            
-            
-        </div>
-    );
+        );
   }
 
 export default Search
