707f10a4db8ee0be3ed0d420cecf4f1631b17a4a
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-11-10 10:02:29 +0100

    :fire: suppression des consoles log useless

---
+++ src/components/Player/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import styles from './Player.module.css'
+
+
+const Player = () => {
+   
+  
+  
+  return (
+    <div className={styles.menu_container}>
+        aaaa
+    </div>
+
+        
+  )
+  
+}
+
+export default Player
