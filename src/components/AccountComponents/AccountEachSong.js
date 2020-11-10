8e4a19b6fbfcc149428d080597166a27900fe5bd
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-11-10 13:02:51 +0100

    :construction: je finis bientot le composant account

--- src/components/AccountComponents/AccountEachSong.js
+++ src/components/AccountComponents/AccountEachSong.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import profilImg from './img/profil.jpg'
 import styles from './Account.module.css'
+import Like from '../LikePause/Like'
 
 
 
@@ -8,16 +9,28 @@ const AccountEachSong = () => {
     
     function Eachsong(props) {
         return(
-            <div className={styles.account_each_song_container}></div>
+            <div className={styles.account_each_song_container}>
+                <div className={styles.account_song_artiste_container}>
+                    <img className={styles.account_music_img} src={props.img}/>
+                    <div>
+                        <p>{props.title}</p>
+                        <p>{props.artist}</p>
+                    </div>
+                </div>
+                <div  className={styles.account_ecoute_like_container}>
+                    <p>{props.ecoute}</p>
+                    <Like />
+                </div>
+            </div>
         )
     }
  
 
     return (
         <div className={styles.account_song_container}>
-            <Eachsong img="a" title="Sara" artist="artist" ecoute="180" />
-            <Eachsong img="a" title="Cahal" artist="artist" ecoute="180" />
-            <Eachsong img="a" title="Edite" artist="artist" ecoute="180" />
+            <Eachsong img={profilImg} title="Sara" artist="artist" ecoute="180" />
+            <Eachsong img={profilImg} title="Cahal" artist="artist" ecoute="180" />
+            <Eachsong img={profilImg} title="Edite" artist="artist" ecoute="180" />
         </div>
 
         
