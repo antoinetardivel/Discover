7702c5ab7e96b1bcd14de3e129d4f2df4ffe1949
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-11-11 01:02:32 +0100

    :construction: le player dans le player composant

--- src/components/Player/PlayerProgressionBar.js
+++ src/components/Player/PlayerProgressionBar.js
@@ -9,10 +9,10 @@ class PlayerProgressionBar extends React.Component {
     render(){
         
         return (
-            <div id="container">
-                <svg id="svg" width="200" height="200" viewPort="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                    <circle r="90" cx="100" cy="100" fill="transparent" stroke="#fff" stroke-width="3" stroke-dasharray="565" stroke-dashoffset="100"></circle>
-                    <circle id="bar" r="90" cx="100" cy="100" fill="transparent" stroke-dasharray="565.48" stroke-dashoffset="0"></circle>
+            <div className={styles.demo}>
+                <svg className={styles.progress} width="250" height="250" viewBox="0 0 120 120">
+                    <circle cx="60" cy="60" r="54" fill="none" stroke="#28f299" stroke-width="3" />
+                    <circle className={styles.progress__value} cx="60" cy="60" r="54" fill="none" stroke="#707070" stroke-width="3" />
                 </svg>
             </div>
         )
