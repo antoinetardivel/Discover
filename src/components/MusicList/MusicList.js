9a96b87b9ac99430c5a803a9aac070e5847a7f95
Maelle05 <maelle.rabouan@gmail.com>
2020-11-12 15:02:10 +0100

    navigation page artist

---
+++ src/components/MusicList/MusicList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './MusicList.module.css'
+
+
+
+function MusicList() {
+
+    return (
+        <div>
+            liste de musiques
+        </div>
+    )
+}
+
+
+  export default MusicList
